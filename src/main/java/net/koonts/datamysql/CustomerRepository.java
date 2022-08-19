759f33874ca42b29e811cf2fdfd3fb34f7ea0e05
James Koonts <jkoontsiii@gmail.com>
2022-08-19 15:29:55 -0400

    .

---
+++ src/main/java/net/koonts/datamysql/CustomerRepository.java
@@ -0,0 +1,8 @@
+package net.koonts.datamysql;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+}
--- src/main/java/net/koonts/datamysql/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package net.koonts.datamysql;
-
-
-import org.springframework.data.repository.CrudRepository;
-import net.koonts.datamysql.Customer;
-
-public interface UserRepository extends CrudRepository<Customer, Integer> {
-
-}
