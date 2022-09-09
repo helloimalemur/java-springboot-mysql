50a52c153ebe0b79004012d7e09b46e4289f84f7
James Koonts <jkoontsiii@gmail.com>
2022-09-08 22:37:08 -0400

    pkgbuild

--- src/main/java/config/usr/share/java-springboot-mysql/start.sh
+++ src/main/java/config/usr/share/java-springboot-mysql/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pkill -xf "Example Main"
-cd /usr/share/example/
-
+cd /usr/share/java-springboot-mysql/
 exec -a 'java-springboot-mysql' /usr/share/java-springboot-mysql/bin/java-springboot-mysql
 
